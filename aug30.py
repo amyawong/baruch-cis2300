690bf0f9fc2e6f0c2b7fa95ceb318ec21d892bf3
amyawong <103911065+amyawong@users.noreply.github.com>
2022-08-30 20:47:47 -0400

    tuesday notes

---
+++ aug30.py
@@ -0,0 +1,28 @@
+# ABSTRACTION
+# look at a problem and try to hone in on essence of it
+
+# DECOMPOSITION
+# break problem into smaller algorithm
+
+# DIVIDE AND CONQUER
+# take a complicated problem and break it down into smaller subproblems, solve smaller subproblems, then put solutions into a whole
+
+# 3-bit numbers
+# 000 -> 0
+# 001 -> 1
+# 010 -> 2
+# 011 -> 3
+# 100 -> 4
+# 101 -> 5
+# 110 -> 6
+# 111 -> 7
+
+# base 2 (binary) system
+# 11 => 1*(2^1) + 1*(2^1) => 2 +1 => 3
+# 110 => 1*(2^2) + 1*(2^1) + 0*(2^0) => 4 + 2 + 0 => 6
+
+# base 10 system
+# 19 => 1*10 + 9*1
+# 49 => 4*10 + 9*1
+# 178 => 1*100 + 7*10 + 1*8
+# 5780 => 5*1000 + 7*100 + 8*10 + 0*1 => 5*10^3 + 7*10^2 + 7*10^1 + 0*10^0
