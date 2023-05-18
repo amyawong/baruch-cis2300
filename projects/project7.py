a72068f569a3a36e1efc59d4b4ac5e232cdd4cf7
Amy <amyannnwong@gmail.com>
2023-05-18 17:06:46 -0400

    project 7

---
+++ projects/project7.py
@@ -0,0 +1,219 @@
+list_of_names = \
+['MARY', 'PATRICIA', 'LINDA', 'BARBARA', 'ELIZABETH', 'JENNIFER', 'MARIA',
+'SUSAN', 'MARGARET', 'DOROTHY', 'LISA', 'NANCY', 'KAREN', 'BETTY', 'HELEN',
+'SANDRA', 'DONNA', 'CAROL', 'RUTH', 'SHARON', 'MICHELLE', 'LAURA', 'SARAH',
+'KIMBERLY', 'DEBORAH', 'JESSICA', 'SHIRLEY', 'CYNTHIA', 'ANGELA', 'MELISSA',
+'BRENDA', 'AMY', 'ANNA', 'REBECCA', 'VIRGINIA', 'KATHLEEN', 'PAMELA',
+'MARTHA', 'DEBRA', 'AMANDA', 'STEPHANIE', 'CAROLYN', 'CHRISTINE', 'MARIE',
+'JANET', 'CATHERINE', 'FRANCES', 'ANN', 'JOYCE', 'DIANE', 'ALICE', 'JULIE',
+'HEATHER', 'TERESA', 'DORIS', 'GLORIA', 'EVELYN', 'JEAN', 'CHERYL', 'MILDRED',
+'KATHERINE', 'JOAN', 'ASHLEY', 'JUDITH', 'ROSE', 'JANICE', 'KELLY', 'NICOLE',
+'JUDY', 'CHRISTINA', 'KATHY', 'THERESA', 'BEVERLY', 'DENISE', 'TAMMY',
+'IRENE', 'JANE', 'LORI', 'RACHEL', 'MARILYN', 'ANDREA', 'KATHRYN', 'LOUISE',
+'SARA', 'ANNE', 'JACQUELINE', 'WANDA', 'BONNIE', 'JULIA', 'RUBY', 'LOIS',
+'TINA', 'PHYLLIS', 'NORMA', 'PAULA', 'DIANA', 'ANNIE', 'LILLIAN', 'EMILY',
+'ROBIN', 'PEGGY', 'CRYSTAL', 'GLADYS', 'RITA', 'DAWN', 'CONNIE', 'FLORENCE',
+'TRACY', 'EDNA', 'TIFFANY', 'CARMEN', 'ROSA', 'CINDY', 'GRACE', 'WENDY',
+'VICTORIA', 'EDITH', 'KIM', 'SHERRY', 'SYLVIA', 'JOSEPHINE', 'THELMA',
+'SHANNON', 'SHEILA', 'ETHEL', 'ELLEN', 'ELAINE', 'MARJORIE', 'CARRIE',
+'CHARLOTTE', 'MONICA', 'ESTHER', 'PAULINE', 'EMMA', 'JUANITA', 'ANITA',
+'RHONDA', 'HAZEL', 'AMBER', 'EVA', 'DEBBIE', 'APRIL', 'LESLIE', 'CLARA',
+'LUCILLE', 'JAMIE', 'JOANNE', 'ELEANOR', 'VALERIE', 'DANIELLE', 'MEGAN',
+'ALICIA', 'SUZANNE', 'MICHELE', 'GAIL', 'BERTHA', 'DARLENE', 'VERONICA',
+'JILL', 'ERIN', 'GERALDINE', 'LAUREN', 'CATHY', 'JOANN', 'LORRAINE', 'LYNN',
+'SALLY', 'REGINA', 'ERICA', 'BEATRICE', 'DOLORES', 'BERNICE', 'AUDREY',
+'YVONNE', 'ANNETTE', 'JUNE', 'SAMANTHA', 'MARION', 'DANA', 'STACY', 'ANA',
+'RENEE', 'IDA', 'VIVIAN', 'ROBERTA', 'HOLLY', 'BRITTANY', 'MELANIE', 'LORETTA',
+'YOLANDA', 'JEANETTE', 'LAURIE', 'KATIE', 'KRISTEN', 'VANESSA', 'ALMA',
+'SUE', 'ELSIE', 'BETH', 'JEANNE', 'VICKI', 'CARLA', 'TARA', 'ROSEMARY',
+'EILEEN', 'TERRI', 'GERTRUDE', 'LUCY', 'TONYA', 'ELLA', 'STACEY', 'WILMA',
+'GINA', 'KRISTIN', 'JESSIE', 'NATALIE', 'AGNES', 'VERA', 'WILLIE', 'CHARLENE',
+'BESSIE', 'DELORES', 'MELINDA', 'PEARL', 'ARLENE', 'MAUREEN', 'COLLEEN',
+'ALLISON', 'TAMARA', 'JOY', 'GEORGIA', 'CONSTANCE', 'LILLIE', 'CLAUDIA',
+'JACKIE', 'MARCIA', 'TANYA', 'NELLIE', 'MINNIE', 'MARLENE', 'HEIDI', 'GLENDA',
+'LYDIA', 'VIOLA', 'COURTNEY', 'MARIAN', 'STELLA', 'CAROLINE', 'DORA',
+'JO', 'VICKIE', 'MATTIE', 'TERRY', 'MAXINE', 'IRMA', 'MABEL', 'MARSHA',
+'MYRTLE', 'LENA', 'CHRISTY', 'DEANNA', 'PATSY', 'HILDA', 'GWENDOLYN',
+'JENNIE', 'NORA', 'MARGIE', 'NINA', 'CASSANDRA', 'LEAH', 'PENNY', 'KAY',
+'PRISCILLA', 'NAOMI', 'CAROLE', 'BRANDY', 'OLGA', 'BILLIE', 'DIANNE',
+'TRACEY', 'LEONA', 'JENNY', 'FELICIA', 'SONIA', 'MIRIAM', 'VELMA', 'BECKY',
+'BOBBIE', 'VIOLET', 'KRISTINA', 'TONI', 'MISTY', 'MAE', 'SHELLY', 'DAISY',
+'RAMONA', 'SHERRI', 'ERIKA', 'KATRINA', 'CLAIRE', 'LINDSEY', 'LINDSAY',
+'GENEVA', 'GUADALUPE', 'BELINDA', 'MARGARITA', 'SHERYL', 'CORA', 'FAYE',
+'ADA', 'NATASHA', 'SABRINA', 'ISABEL', 'MARGUERITE', 'HATTIE', 'HARRIET',
+'MOLLY', 'CECILIA', 'KRISTI', 'BRANDI', 'BLANCHE', 'SANDY', 'ROSIE', 'JOANNA',
+'IRIS', 'EUNICE', 'ANGIE', 'INEZ', 'LYNDA', 'MADELINE', 'AMELIA', 'ALBERTA',
+'GENEVIEVE', 'MONIQUE', 'JODI', 'JANIE', 'MAGGIE', 'KAYLA', 'SONYA', 'JAN',
+'LEE', 'KRISTINE', 'CANDACE', 'FANNIE', 'MARYANN', 'OPAL', 'ALISON', 'YVETTE',
+'MELODY', 'LUZ', 'SUSIE', 'OLIVIA', 'FLORA', 'SHELLEY', 'KRISTY', 'MAMIE',
+'LULA', 'LOLA', 'VERNA', 'BEULAH', 'ANTOINETTE', 'CANDICE', 'JUANA', 'JEANNETTE',
+'PAM', 'KELLI', 'HANNAH', 'WHITNEY', 'BRIDGET', 'KARLA', 'CELIA', 'LATOYA',
+'PATTY', 'SHELIA', 'GAYLE', 'DELLA', 'VICKY', 'LYNNE', 'SHERI', 'MARIANNE',
+'KARA', 'JACQUELYN', 'ERMA', 'BLANCA', 'MYRA', 'LETICIA', 'PAT', 'KRISTA',
+'ROXANNE', 'ANGELICA', 'JOHNNIE', 'ROBYN', 'FRANCIS', 'ADRIENNE', 'ROSALIE',
+'ALEXANDRA', 'BROOKE', 'BETHANY', 'SADIE', 'BERNADETTE', 'TRACI', 'JODY',
+'KENDRA', 'JASMINE', 'NICHOLE', 'RACHAEL', 'CHELSEA', 'MABLE', 'ERNESTINE',
+'MURIEL', 'MARCELLA', 'ELENA', 'KRYSTAL', 'ANGELINA', 'NADINE', 'KARI',
+'ESTELLE', 'DIANNA', 'PAULETTE', 'LORA', 'MONA', 'DOREEN', 'ROSEMARIE',
+'ANGEL', 'DESIREE', 'ANTONIA', 'HOPE', 'GINGER', 'JANIS', 'BETSY', 'CHRISTIE',
+'FREDA', 'MERCEDES', 'MEREDITH', 'LYNETTE', 'TERI', 'CRISTINA', 'EULA',
+'LEIGH', 'MEGHAN', 'SOPHIA', 'ELOISE', 'ROCHELLE', 'GRETCHEN', 'CECELIA',
+'RAQUEL', 'HENRIETTA', 'ALYSSA', 'JANA', 'KELLEY', 'GWEN', 'KERRY', 'JENNA',
+'TRICIA', 'LAVERNE', 'OLIVE', 'ALEXIS', 'TASHA', 'SILVIA', 'ELVIRA', 'CASEY',
+'DELIA', 'SOPHIE', 'KATE', 'PATTI', 'LORENA', 'KELLIE', 'SONJA', 'LILA',
+'LANA', 'DARLA', 'MAY', 'MINDY', 'ESSIE', 'MANDY', 'LORENE', 'ELSA', 'JOSEFINA',
+'JEANNIE', 'MIRANDA', 'DIXIE', 'LUCIA', 'MARTA', 'FAITH', 'LELA', 'JOHANNA',
+'SHARI', 'CAMILLE', 'TAMI', 'SHAWNA', 'ELISA', 'EBONY', 'MELBA', 'ORA',
+'NETTIE', 'TABITHA', 'OLLIE', 'JAIME', 'WINIFRED', 'KRISTIE']
+
+
+
+# NOTE: make sure unique_word string is in uppercase
+
+
+# Method 1A: Nested for-loops
+unique_word = ''
+# unique_word = unique_word.upper()  # either uncomment this line OR line 82 if unique_word is in lowercase
+
+list_A, list_B = [], []
+A, B = 0, 0
+
+for i in range(0, len(list_of_names)):
+    # list_of_names[i] = list_of_names[i].lower()  # uncomment this line OR line 76 if unique_word is in lowercase
+
+    for j in range(0, len(unique_word)):
+        if unique_word[j] in list_of_names[i]:
+            list_A.append(list_of_names[i])
+            list_A = list(set(list_A))
+            A = len(list_A)
+        if list_of_names[i][-1] == unique_word[j]:
+            list_B.append(list_of_names[i])
+            list_B = list(set(list_B))
+            B = len(list_B)
+
+print(A)
+print(B)
+print(A + B)
+
+
+
+# Method 1B: Nested for-loops
+A, B = 0, 0
+for word in list_of_names:
+    for letter in unique_word:
+      if letter in word:
+        A += 1
+    last_letter = word[-1]
+    if last_letter in unique_word:
+        B += 1
+
+print(A)
+print(B)
+print(A + B)
+
+
+
+
+
+# Method 2A: Hard-coded solution using A = 0 and B = 0
+A, B = 0, 0
+for name in list_of_names:
+  if name.count('') >= 1 or name.count('') >= 1 or name.count('') >= 1 or name.count('') >= 1 or name.count('') >= 1 or name.count('') >= 1 or name.count(''):
+    A += 1
+    
+  if name[-1] == ''or name[-1] == '' or name[-1] == '' or name[-1] == '' or name[-1] == '' or name[-1] == '' or name[-1] == '':
+    B += 1
+
+print(A)
+print(B)
+print(A + B)
+
+
+
+# Method 2B: Hard-coded solution using a_b = [0, 0]
+a_b = [0, 0]
+for name in list_of_names:
+  if name.count('') >= 1 or name.count('') >= 1 or name.count('') >= 1 or name.count('') >= 1 or name.count('') >= 1 or name.count('') >= 1 or name.count(''):
+    a_b[0] += 1
+    
+  if name[-1] == ''or name[-1] == '' or name[-1] == '' or name[-1] == '' or name[-1] == '' or name[-1] == '' or name[-1] == '':
+    a_b[1] += 1
+
+print(a_b[0] + a_b[1])
+
+
+
+
+# Method 3: Using sets
+unique_word = ''
+unique_word = unique_word.upper()
+A, B = 0, 0
+
+for word in list_of_names:
+    word_letter_set = set(word)  # turns each name in list into a set where each value is a unique letter in the name
+    my_letter_set = set(unique_word)  # turns unique word into a set with duplicate letters filtered out
+
+    if len(word_letter_set.intersection(my_letter_set)) > 0:  # compares each letter from unique word with each letter in a set of name
+        A += 1
+    
+    if word[-1] in unique_word:
+        B += 1
+
+print(A)
+print(B)
+print(A + B)
+
+
+
+
+
+
+# NOTE: these solutions do NOT work because they count duplicates
+"""
+unique_word = ''
+for word in list_of_names:
+    if '' in word or '' in word or '' in word or '' in word or '' in word or '' in word or '' in word:
+        A += 1
+    if word[-1] in unique_word:
+        B += 1
+print(A)
+print(B)
+print(A + B)
+"""
+
+"""
+# Hard-coded solution using a bunch of if...elif statements
+for word in list_of_names:
+    if '' in word:
+        A += 1
+    elif '' in word:
+        A += 1
+    elif '' in word:
+        A += 1
+    elif '' in word:
+        A += 1
+    elif '' in word:
+        A += 1
+    elif '' in word:
+        A += 1
+    elif '' in word:
+        A += 1
+
+    if word[-1] == '':
+        B += 1
+    if word[-1] == '':
+        B += 1
+    if word[-1] == '':
+        B += 1
+    if word[-1] == '':
+        B += 1
+    if word[-1] == '':
+        B += 1
+    if word[-1] == '':
+        B += 1
+    if word[-1] == '':
+        B += 1
+print(A)
+print(B)
+print(A + B)
+"""
